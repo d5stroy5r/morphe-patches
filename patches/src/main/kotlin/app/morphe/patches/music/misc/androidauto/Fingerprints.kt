e42d9c1bbdd6985378be47c8f9138ccdb22ead1e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-04 11:40:25 +0100

    feat(YouTube Music): Add experimental support for `9.09.52`

--- patches/src/main/kotlin/app/morphe/patches/music/misc/androidauto/Fingerprints.kt
+++ patches/src/main/kotlin/app/morphe/patches/music/misc/androidauto/Fingerprints.kt
@@ -1,12 +1,20 @@
+/*
+ * Copyright 2026 Morphe.
+ * https://github.com/MorpheApp/morphe-patches
+ *
+ * Original hard forked code:
+ * https://github.com/ReVanced/revanced-patches/commit/724e6d61b2ecd868c1a9a37d465a688e83a74799
+ */
+
 package app.morphe.patches.music.misc.androidauto
 
 import app.morphe.patcher.Fingerprint
 
 internal object CheckCertificateFingerprint : Fingerprint(
     returnType = "Z",
-    parameters = listOf("Ljava/lang/String;"),
+    parameters = listOf("L"),
     strings = listOf(
         "X509",
-        "Failed to get certificate" // Partial String match.
+        "isPartnerSHAFingerprint"
     )
 )
